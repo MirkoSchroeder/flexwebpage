6b84bb5a3fa7c91f2c329ce899f8ca791a795429
Mirko <mirschroeder@hotmail.com>
2023-03-08 00:40:44 -0300

    finish

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import './App.css';
+import "./App.css";
 import { Route, Routes } from "react-router-dom";
-import HomePage from './Pages/HomePage/HomePage';
-import About from './Pages/About/About';
+import HomePage from "./Pages/HomePage/HomePage";
+import About from "./Pages/About/About";
 
 function App() {
   return (
-    <div className='h-screen bg-violeta-800'>
+    <div className="h-screen bg-violeta-800">
       <Routes>
-        <Route path="/home" element={<HomePage />} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<About />} />
       </Routes>
-      
     </div>
   );
 }
